c910c33f266ad4a74d28ca1dada51f0ec8b65c3a
karuta0825 <magune3g@gmail.com>
2022-08-10 00:32:20 +0900

    update

--- atcoder/abc263/c.py
+++ atcoder/abc263/c.py
@@ -1,22 +1,18 @@
 N, M = list(map(int, input().split()))
 
-
-N, M = [3,5]
 ary = [i for i in range(1, M +1)]
-for i in range(1, M+1):
-    for j in range(1, M+1):
-        if (i < j):
-            print(i,j)
-            
+
+r = []
 for i in range(1 << M):
-    str = format(i, 'b').zfill(M)
-    
-    if (len(list(filter(lambda v: v == '1', str))) == 3):
-         print(str);
-         
+    s = format(i, 'b').zfill(M)
 
+    a = []
+    for idx, c in enumerate(s):
+        if (c == '1'):
+            a.append(ary[idx])
 
-for i in "00111":
+    if (len(a) == N):
+        r.append(a)
 
-
-
+for i in sorted(r):
+    print(" ".join(map(str, i)))
