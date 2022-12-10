05b07092717a3fca5f3b9af417486a595343718b
karuta0825 <magune3g@gmail.com>
2022-12-11 00:00:03 +0900

    update

--- atcoder/abc281/b.py
+++ atcoder/abc281/b.py
@@ -1,7 +1,7 @@
 import re
 S = input()
 
-p = re.compile("{}".format("[A-Z][1-9]{1}[0-9]*[A-Z]"))
+p = re.compile("{}".format("[A-Z][1-9][0-9]*[A-Z]"))
 
 # p = re.compile("".format("(?P<A>[A-Z])(?P<V>[0-9]{6})(?P<B>[A-Z])"))
 
@@ -13,11 +13,15 @@ p = re.compile("{}".format("[A-Z][1-9]{1}[0-9]*[A-Z]"))
 result = "Yes"
 if (re.match(p, S)):
     length = len(S)
-    num = int(S[1:length-1])
-    if (100000 <= num <= 999999):
-        result = "Yes"
-    else:
+    v = S[1:length-1]
+    if (not v.isdigit()):
         result = "No"
+    else:
+        num = int(v)
+        if (100000 <= num <= 999999):
+            result = "Yes"
+        else:
+            result = "No"
 else:
     result = "No"
 
