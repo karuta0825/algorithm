0391dfdacf238de3425c331344401aa38dd910d9
karuta0825 <magune3g@gmail.com>
2022-12-23 21:29:14 +0900

    add tessoku-book a11

---
+++ atcoder/tessosku-book/a11.py
@@ -0,0 +1,30 @@
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+L = 0
+R = len(A)
+
+# 1. while文でとく
+# result = True
+# while result:
+#     middle = (left + right )//2
+#     if (A[middle] > X):
+#         right = middle
+#     elif (A[middle] < X):
+#         left = middle
+#     else:
+#         result = False
+#         print(middle + 1)
+
+# 2. 再帰関数でとく
+# どうして左右の移動で偶数奇数問題はないのだろうか？とおもう。
+def search(left, right):
+    middle = (left + right) // 2
+    if (A[middle] > X):
+        return search(left, middle)
+    elif (A[middle] < X):
+        return search(middle, right)
+    else:
+        return middle + 1
+
+print(search(L, R))
