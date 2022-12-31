962089e447359068e1154748030deb02d22455be
karuta0825 <magune3g@gmail.com>
2022-12-31 23:14:15 +0900

    update

--- atcoder/tessosku-book/a19.py
+++ atcoder/tessosku-book/a19.py
@@ -1,27 +1,24 @@
 N, W = map(int, input().split())
 WV = [list(map(int, input().split())) for _ in range(N)]
 
-sumW = 0
-for wv in WV:
-    sumW += wv[0]
-    
+# これがアウト！それぞれの足し算よりも大きい数字がWになることがあるため
+# sumW = 0
+# for wv in WV:
+#     sumW += wv[0]
+
 # i \ w
 # dp[i][w] = max(dp[i-1][w], dp[i-1][w-W[i]])
 
-dp = [[0] * (sumW+1) for _ in range(N+1)]
+dp = [[0] * (W+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     [wgt,val] = WV[i-1]
 
-    for w in range(sumW+1):
+    for w in range(W+1):
+
         if (w - wgt >= 0):
             dp[i][w] = max(dp[i-1][w], dp[i-1][w-wgt] + val)
         else:
             dp[i][w] = dp[i-1][w]
 
-# for x in range(len(dp)):
-#     print(dp[x])
-    
 print(dp[N][W])
- 
-
