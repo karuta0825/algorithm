7ecd48ea7bfc61cca8fb71a4801e0e24510b71e6
karuta0825 <magune3g@gmail.com>
2023-01-08 19:27:23 +0900

    add abc284 d

---
+++ atcoder/abc284/d.py
@@ -0,0 +1,53 @@
+import math
+T = int(input())
+
+
+def searchPQ(n):
+    i = 2
+    p = 0
+    q = 0
+    while i ** 3 <= n:
+
+        if (not (n % i == 0)):
+            i += 1
+            continue
+
+        if ((n / i) % i == 0):
+            p = i
+            q = int(n / i / i)
+        else:
+            q = i
+            p = int(math.sqrt(n/i))
+
+        break
+
+    return [p, q]
+
+# なぜqが1通りに決まるのか？
+# それは素数だからである。そういうことだな。
+# あぁなるほど。素数をそういう意味に使うのか！
+# これは悔しいな。素数かどうかの判定ではなくて割り切れないという特徴なんだ。
+def searchPQ2(n):
+
+    for i in range(2, int(n ** 1/3)):
+
+        if (not (n % i == 0)):
+            continue
+
+        if ((n / i) % i == 0):
+            p = i
+            q = int(n / i / i)
+        else:
+            q = i
+            p = int(math.sqrt(n/i))
+
+        break
+
+    return [p, q]
+
+
+
+for _ in range(T):
+
+    N = int(input())
+    print(*searchPQ2(N))
