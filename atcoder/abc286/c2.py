63e32c07a3c79cbd653b2b7d1eeabf3b2a4aea16
karuta0825 <magune3g@gmail.com>
2023-01-23 08:38:35 +0900

    update abc286 c

--- atcoder/abc286/c2.py
+++ atcoder/abc286/c2.py
@@ -2,11 +2,11 @@ N, A, B = map(int, input().split())
 s = input()
 inits = s[:]
 
-# 
+#
 def a(s):
     l = s[0]
     return s[1:] + l
-    
+
 
 def diff(s, t):
     cnt = 0
@@ -23,26 +23,36 @@ def cost(s):
 
     if (length % 2 == 1):
         left = s[0:mid]
-        right = s[mid+1:length]
+        right = s[mid+1:]
     else:
         left = s[0:mid]
-        right = s[mid:length]
+        right = s[mid:]
 
     return diff(left, right)
-    
+
 # まずN回やってみて一番コストがさがったときをさがす
-# ええところきてるのにな。
-mincost = cost(s)
-num = 0
-for i in range(len(s)):
+# このやり方がまずいのは最小のがあるかもしれないことだよな。
+# mincost = cost(s)
+# num = 0
+# for i in range(len(s)):
+#     s = a(s)
+#     newcost = cost(s)
+#     if (newcost < mincost):
+#         mincost = newcost
+#         num = i + 1
+
+# for _ in range(num):
+#     s = a(s)
+
+# print(min((num * A + cost(s) * B), cost(inits) * B))
+# 全パターンから計算すればよいのか！よしといたぞ。
+result = cost(inits) * B
+length = len(s)
+for i in range(length-1):
+
     s = a(s)
-    newcost = cost(s)
-    if (newcost < mincost):
-        mincost = newcost
-        num = i + 1 
-
-for _ in range(num):
-    s = a(s)    
+    ans = (i+1) * A + cost(s) * B
+    result = min(ans, result)
 
 
-print(min((num * A + cost(s) * B), cost(inits) * B))
+print(result)
