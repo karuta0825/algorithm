63abf00e5616965d5926bb3192c908c705cd50bb
karuta0825 <magune3g@gmail.com>
2023-07-18 22:02:08 +0900

    add tessoku-book A19

---
+++ atcoder/tessosku-book/A19/2023-07-18_21-46.py
@@ -0,0 +1,19 @@
+N, W = map(int, input().split())
+
+items = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (W+1) for _ in range(N+1)]
+
+
+for i in range(1,N+1):
+    for j in range(W+1):
+
+        # i-1しないとindexがずれるってことが大事
+        w, v = items[i-1]
+
+        dp[i][j] = dp[i-1][j]
+
+        if j - w >= 0:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-w]+v)
+
+print(dp[N][W])
